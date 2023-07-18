e8d64f1c5150811068f10562cf0d37242e63026e
bkarnecki <bkarnecki@genesis.com.mt>
2023-07-18 18:54:31 +0200

    add tests for not valid input

--- src/test/java/pl/karnecki/auctane/StringToIntArrayConverterTest.java
+++ src/test/java/pl/karnecki/auctane/StringToIntArrayConverterTest.java
@@ -2,9 +2,11 @@ package pl.karnecki.auctane;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import pl.karnecki.auctane.exceptions.InvalidInputException;
 import pl.karnecki.auctane.utils.StringToIntArrayConverter;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class StringToIntArrayConverterTest {
 
@@ -17,5 +19,16 @@ class StringToIntArrayConverterTest {
         int[] expected = {1, 2, 3, 4, 5};
         int[] result = StringToIntArrayConverter.convert(input);
         assertArrayEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "",
+        "1, 2, abc, 4, 5",
+        "a, b, c, d, e",
+        "1, a, 2, b, 3, c"})
+    void throwInvalidInputExceptionWhenInvalidInput(String input) {
+        assertThrows(InvalidInputException.class, () ->
+            StringToIntArrayConverter.convert(input));
     }
 }
