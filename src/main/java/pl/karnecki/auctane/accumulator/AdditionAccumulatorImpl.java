0f3a2aaf6eed3bafe7fe8051f079aeb5a0f52fbc
bkarnecki <bkarnecki@genesis.com.mt>
2023-07-19 17:21:28 +0200

    Refactor accumulate() in case of reaching edge cases

--- src/main/java/pl/karnecki/auctane/accumulator/AdditionAccumulatorImpl.java
+++ src/main/java/pl/karnecki/auctane/accumulator/AdditionAccumulatorImpl.java
@@ -1,19 +1,38 @@
 package pl.karnecki.auctane.accumulator;
 
-import java.util.Arrays;
+import pl.karnecki.auctane.utils.ConsoleLogger;
 
 public class AdditionAccumulatorImpl implements Accumulator {
+    private final ConsoleLogger consoleLogger;
     private int total;
 
+
     public AdditionAccumulatorImpl() {
+        this.consoleLogger = new ConsoleLogger();
         this.total = 0;
     }
 
     @Override
     public int accumulate(int... values) {
-        var sum = Arrays.stream(values).sum();
+        var sum = 0;
+
+        for (int value : values) {
+            if (isMaxIntReached(sum, value) || isMinIntReached(sum, value)) {
+                consoleLogger.logWarnMessage("Overflow occurred - data might be lost");
+                return sum;
+            }
+            sum += value;
+        }
         total += sum;
         return sum;
+    }
+
+    private static boolean isMinIntReached(final int sum, final int value) {
+        return value < 0 && sum < Integer.MIN_VALUE - value;
+    }
+
+    private static boolean isMaxIntReached(final int sum, final int value) {
+        return value > 0 && sum > Integer.MAX_VALUE - value;
     }
 
     @Override
