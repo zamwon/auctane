9b42901d567672cc1fa26eaff8024fb31c02b279
bkarnecki <bkarnecki@genesis.com.mt>
2023-07-20 11:22:02 +0200

    refactor test convertValidInput

--- src/test/java/pl/karnecki/auctane/StringToIntArrayConverterTest.java
+++ src/test/java/pl/karnecki/auctane/StringToIntArrayConverterTest.java
@@ -1,8 +1,12 @@
 package pl.karnecki.auctane;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import pl.karnecki.auctane.utils.StringToIntArrayConverter;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -10,14 +14,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class StringToIntArrayConverterTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {
-        "1, 2, 3, 4, 5",
-        "1 2 3 4 5",
-        "1,2,3,4,5"})
-    void convertValidInput(String input) {
-
-        //given
-        var expected = new int[]{1, 2, 3, 4, 5};
+    @MethodSource("parseStringProvider")
+    void convertValidInput(int[] expected, String input) {
 
         //when
         var result = StringToIntArrayConverter.convert(input);
@@ -30,10 +28,23 @@ class StringToIntArrayConverterTest {
     @ValueSource(strings = {
         "",
         "1, 2, abc, 4, 5",
-        "a, b, c, d, e",
-        "1, a, 2, b, 3, c"})
+        "a, b, c, d, e, f",
+        "1; a; 2; b; 3; c",
+        "1 2 3 a b c d e f 1 2 3"}
+    )
     void throwInvalidInputExceptionWhenInvalidInput(String input) {
         assertThrows(NumberFormatException.class, () ->
             StringToIntArrayConverter.convert(input));
+    }
+
+    private static Stream<Arguments> parseStringProvider() {
+        return Stream.of(
+            Arguments.of(new int[] {1, 2, 3, 4, 5, 6} ,"1, 2, 3, 4, 5, 6"),
+            Arguments.of(new int[] {1, 2, 4, 5} ,"1 2 4 5"),
+            Arguments.of(new int[] {1, 2, 3, 4, 5, 6, 7} ,"1, 2; 3 4,5;6;7"),
+            Arguments.of(new int[] {1, 2, 3, 4, 5} ,"1;2;3;4;5"),
+            Arguments.of(new int[] {1, 2, 3, 4, 5} ,"1; 2; 3; 4; 5"),
+            Arguments.of(new int[] {1, 2, 3, 4, 5} ,"1,2,3,4,5")
+        );
     }
 }
