36c54050b4b8886381bc59fd5926ce27331fff09
bkarnecki <bkarnecki@genesis.com.mt>
2023-07-20 10:06:17 +0200

    add edge test cases

--- src/test/java/pl/karnecki/auctane/AdditionAccumulatorTest.java
+++ src/test/java/pl/karnecki/auctane/AdditionAccumulatorTest.java
@@ -22,7 +22,7 @@ class AdditionAccumulatorTest {
     }
 
     @ParameterizedTest
-    @MethodSource("provideTestCases")
+    @MethodSource("accumulatePositiveCases")
     void shouldAccumulate(int expectedSum, int... values) {
 
         //when
@@ -36,7 +36,7 @@ class AdditionAccumulatorTest {
     void shouldGetTotal() {
 
         //given
-        var prompt1 = new int[]{1,2,3};
+        var prompt1 = new int[]{1, 2, 3};
         var prompt2 = new int[]{4};
 
         //when
@@ -52,7 +52,7 @@ class AdditionAccumulatorTest {
     void shouldResetAccumulator() {
 
         //given
-        var prompt1 = new int[]{1,2,3};
+        var prompt1 = new int[]{1, 2, 3};
         accumulator.accumulate(prompt1);
 
         //when
@@ -63,11 +63,54 @@ class AdditionAccumulatorTest {
         assertEquals(0, total);
     }
 
-    private static Stream<Arguments> provideTestCases() {
+    @ParameterizedTest
+    @MethodSource("accumulateNegativeNumbersEdgeCase")
+    void shouldNotAccumulateWhenMinIntReached(int expectedSum, int[] values) {
+        int sum = accumulator.accumulate(values);
+        assertEquals(expectedSum, sum);
+        assertEquals(0, accumulator.getTotal());
+    }
+
+    @ParameterizedTest
+    @MethodSource("accumulatePositiveNumbersEdgeCase")
+    void shouldNotAccumulateWhenMaxIntReached(int expectedSum, int[] values) {
+        int sum = accumulator.accumulate(values);
+        assertEquals(expectedSum, sum);
+        assertEquals(0, accumulator.getTotal());
+    }
+
+    private static Stream<Arguments> accumulateNegativeNumbersEdgeCase() {
         return Stream.of(
-            Arguments.of(6, 1, 2, 3),
-            Arguments.of(4, 4),
-            Arguments.of(10, 1, 2, 3, 4)
+            Arguments.of(Integer.MIN_VALUE, new int[]{Integer.MIN_VALUE, -1}),
+            Arguments.of(-2147483548, new int[]{Integer.MIN_VALUE, 100, -101}),
+            Arguments.of(-1, new int[]{-1, Integer.MIN_VALUE}),
+            Arguments.of(-5, new int[]{-2, -3, Integer.MIN_VALUE}),
+            Arguments.of(-70, new int[]{100, -120, -50, Integer.MIN_VALUE}),
+            Arguments.of(Integer.MIN_VALUE, new int[]{Integer.MIN_VALUE, Integer.MIN_VALUE})
+        );
+    }
+
+    private static Stream<Arguments> accumulatePositiveNumbersEdgeCase() {
+        return Stream.of(
+            Arguments.of(Integer.MAX_VALUE, new int[]{Integer.MAX_VALUE, 1}),
+            Arguments.of(2147483547, new int[]{Integer.MAX_VALUE, -100, 101}),
+            Arguments.of(6, new int[]{1, 2, 3, Integer.MAX_VALUE}),
+            Arguments.of(Integer.MAX_VALUE, new int[]{Integer.MAX_VALUE, Integer.MAX_VALUE})
+        );
+    }
+
+    private static Stream<Arguments> accumulatePositiveCases() {
+        return Stream.of(
+            Arguments.of(6, new int[]{1, 2, 3}),
+            Arguments.of(4, new int[]{4}),
+            Arguments.of(10, new int[]{1, 2, 3, 4}),
+            Arguments.of(150, new int[]{15, 30, 45, 60}),
+            Arguments.of(111100, new int[]{100, 1000, 10000, 100000}),
+            Arguments.of(499, new int[]{123, Integer.MIN_VALUE, -123, 500, Integer.MAX_VALUE}),
+            Arguments.of(-2147483618, new int[]{100, -120, 50, -2000, 2000, Integer.MIN_VALUE}),
+            Arguments.of(1, new int[]{1, Integer.MAX_VALUE}),
+            Arguments.of(2147483642, new int[]{-2, -3, Integer.MAX_VALUE}),
+            Arguments.of(2147483577, new int[]{100, -120, -50, Integer.MAX_VALUE})
         );
     }
 }
