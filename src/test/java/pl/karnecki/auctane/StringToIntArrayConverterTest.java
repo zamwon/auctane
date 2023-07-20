22ccbfd9a97ccbb6789995ccbeee5aed643d5b24
bkarnecki <bkarnecki@genesis.com.mt>
2023-07-20 18:23:49 +0200

    add test to convert numbers that doesn't fit integer

--- src/test/java/pl/karnecki/auctane/StringToIntArrayConverterTest.java
+++ src/test/java/pl/karnecki/auctane/StringToIntArrayConverterTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import pl.karnecki.auctane.utils.StringToIntArrayConverter;
 
+import java.math.BigInteger;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
@@ -21,7 +22,18 @@ class StringToIntArrayConverterTest {
         var result = StringToIntArrayConverter.convert(input);
 
         //then
-        assertArrayEquals(expected, result);
+        assertArrayEquals(expected, (int[]) result);
+    }
+
+    @ParameterizedTest
+    @MethodSource("parseStringBigIntProvider")
+    void convertValidBigInput(BigInteger[] expected, String input) {
+
+        //when
+        var result = StringToIntArrayConverter.convert(input);
+
+        //then
+        assertArrayEquals(expected, (BigInteger[]) result);
     }
 
     @ParameterizedTest
@@ -30,8 +42,11 @@ class StringToIntArrayConverterTest {
         "1, 2, abc, 4, 5",
         "a, b, c, d, e, f",
         "1; a; 2; b; 3; c",
-        "1 2 3 a b c d e f 1 2 3"}
-    )
+        "1 2 3 a b c d e f 1 2 3",
+        "1 41241241241241241241; adb",
+        "! @ #",
+        "1 * 5 --5"
+    })
     void throwInvalidInputExceptionWhenInvalidInput(String input) {
         assertThrows(NumberFormatException.class, () ->
             StringToIntArrayConverter.convert(input));
@@ -39,12 +54,30 @@ class StringToIntArrayConverterTest {
 
     private static Stream<Arguments> parseStringProvider() {
         return Stream.of(
-            Arguments.of(new int[] {1, 2, 3, 4, 5, 6} ,"1, 2, 3, 4, 5, 6"),
-            Arguments.of(new int[] {1, 2, 4, 5} ,"1 2 4 5"),
-            Arguments.of(new int[] {1, 2, 3, 4, 5, 6, 7} ,"1, 2; 3 4,5;6;7"),
-            Arguments.of(new int[] {1, 2, 3, 4, 5} ,"1;2;3;4;5"),
-            Arguments.of(new int[] {1, 2, 3, 4, 5} ,"1; 2; 3; 4; 5"),
-            Arguments.of(new int[] {1, 2, 3, 4, 5} ,"1,2,3,4,5")
+            Arguments.of(new int[]{1, 2, 3, 4, 5, 6}, "1, 2, 3, 4, 5, 6"),
+            Arguments.of(new int[]{1, 2, 4, 5}, "1 2 4 5"),
+            Arguments.of(new int[]{1, 2, 3, 4, 5, 6, 7}, "1, 2; 3 4,5;6;7"),
+            Arguments.of(new int[]{1, 2, 3, 4, 5}, "1;2;3;4;5"),
+            Arguments.of(new int[]{1, 2, 3, 4, 5}, "1; 2; 3; 4; 5"),
+            Arguments.of(new int[]{1, 2, 3, 4, 5}, "1,2,3,4,5")
+        );
+    }
+
+    private static Stream<Arguments> parseStringBigIntProvider() {
+        return Stream.of(
+            Arguments.of(new BigInteger[]{BigInteger.valueOf(999999999999999L), BigInteger.valueOf(1)}, "999999999999999; 1"),
+            Arguments.of(new BigInteger[]{BigInteger.valueOf(999999999999999L), BigInteger.valueOf(1)}, "999999999999999, 1"),
+            Arguments.of(new BigInteger[]{BigInteger.valueOf(999999999999999L), BigInteger.valueOf(1)}, "999999999999999 1"),
+            Arguments.of(new BigInteger[]{BigInteger.valueOf(1), BigInteger.valueOf(999999999999999L)}, "1, 999999999999999"),
+            Arguments.of(new BigInteger[]{BigInteger.valueOf(1), BigInteger.valueOf(999999999999999L)}, "1; 999999999999999"),
+            Arguments.of(new BigInteger[]{BigInteger.valueOf(1), BigInteger.valueOf(999999999999999L)}, "1 999999999999999"),
+            Arguments.of(new BigInteger[]{BigInteger.valueOf(1), BigInteger.valueOf(999999999999999L)}, "1,999999999999999"),
+            Arguments.of(new BigInteger[]{BigInteger.valueOf(1), BigInteger.valueOf(999999999999999L)}, "1;999999999999999"),
+            Arguments.of(new BigInteger[]{BigInteger.valueOf(999999999999999L), BigInteger.valueOf(2222222222222L)}, "999999999999999, 2222222222222"),
+            Arguments.of(new BigInteger[]{BigInteger.valueOf(999999999999999L), BigInteger.valueOf(2222222222222L)}, "999999999999999,2222222222222"),
+            Arguments.of(new BigInteger[]{BigInteger.valueOf(999999999999999L), BigInteger.valueOf(2222222222222L)}, "999999999999999;2222222222222"),
+            Arguments.of(new BigInteger[]{BigInteger.valueOf(999999999999999L), BigInteger.valueOf(2222222222222L)}, "999999999999999; 2222222222222"),
+            Arguments.of(new BigInteger[]{BigInteger.valueOf(999999999999999L), BigInteger.valueOf(2222222222222L)}, "999999999999999 2222222222222")
         );
     }
 }
